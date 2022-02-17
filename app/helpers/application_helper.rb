743d526d530f07ce35895c3bfc05f9797f707ae1
MulloaV <mulloa0179@e-camp.cl>
2022-02-17 04:56:36 -0300

    1M Registers w. Kaminari/Pg Search

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,6 @@
+module ApplicationHelper
+
+    def query_array
+        params[:q].to_s.split(' ')
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,37 @@
+100.times do
+    Author.create(name: Faker::Book.author)
+end
+
+100.times do
+    Publisher.create(name: Faker::Book.author)
+end
+
+100.times do
+    Genre.create(name: Faker::Book.genre)
+end
+
+100.times do
+    [].tap do |array|
+        10_000.times do
+            time = Time.current
+            array << {
+                title: Faker::Book.title,
+                author_id: 1 + rand(100),
+                publisher_id: 1 + rand(100),
+                genre_id: 1 + rand(100),
+                updated_at: time,
+                created_at: time
+            }
+       end
+       Book.insert_all(array)
+    end
+end
+
+#1_000_000.times do
+#    Book.create(
+#        title: Faker::Book.title,
+#        author_id: 1 + rand(100),
+#        publisher_id: 1 + rand(100),
+#        genre_id: 1 + rand(100)
+#    )
+#end
